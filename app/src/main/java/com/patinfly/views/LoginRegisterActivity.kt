123d5fc75f0ac75ea5ac5b65302813642118cac6
cristhian fajardo <cristhianfg.cfg@gmail.com>
2024-03-26 13:50:56 +0100

    paso de inicio a login o register

--- app/src/main/java/com/patinfly/views/LoginRegisterActivity.kt
+++ app/src/main/java/com/patinfly/views/LoginRegisterActivity.kt
@@ -1,12 +1,26 @@
 package com.patinfly.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.patinfly.R
+import com.patinfly.databinding.ActivityLoginRegisterBinding
 
 class LoginRegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_register)
+        binding = ActivityLoginRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.loginBt.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.loginBt.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/patinfly/views/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.patinfly.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.patinfly.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+
+    }
+}
